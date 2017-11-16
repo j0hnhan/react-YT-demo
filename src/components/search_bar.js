6787440705683e04824134224b6fea63df2db782
Jiong Han <jiong.han@wustl.edu>
2017-11-16 12:51:03 -0800

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+// const SearchBar = function() {
+// 	return <input />;
+// }
+
+class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: ''};
+	}
+
+	render() {
+		return (
+			<div className="search-bar">
+				<input 
+				value = { this.state.term }
+				onChange = {event => this.onInputChange(event.target.value)}
+				/>
+			</div>
+		);
+	}
+
+	onInputChange(term) {
+		this.setState({term: term});
+		this.props.onSearchTermChange(term);
+	}
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
+import _ from 'lodash';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyBIcP6wEmbQUu8X9L7mNpDrccd63hFijOI';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// const App = function() {
+// 	return (<div>
+// 				<SearchBar />
+// 			</div>);
+// }
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		 	videos: [],
+		 	selectedVideo: null 
+		};
+
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		let that = this;
+		YTSearch({key: API_KEY, term: term}, function(data) {
+			console.log(data);
+			that.setState({ 
+				videos: data,
+				selectedVideo: data[0]
+			});
+		});		
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+		return(
+			<div>
+				<SearchBar onSearchTermChange = {videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={(selectedVideo) => {this.setState({selectedVideo: selectedVideo})}}
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+ReactDom.render(<App />, document.querySelector(".container"));
